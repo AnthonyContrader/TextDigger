6e1c45df0cdd5e1b324d2cbaca7f77ec0c4b9dc6
fellonemarco <65163898+fellonemarco@users.noreply.github.com>
2020-05-22 09:30:15 +0200

    updated db files

--- db/sampledb.sql
+++ db/sampledb.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `sampledb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `sampledb`;
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sampledb
 -- ------------------------------------------------------
--- Server version	8.0.15
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +16,85 @@ USE `sampledb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `document`
+--
+
+DROP TABLE IF EXISTS `document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `document` (
+  `iddocument` int(11) NOT NULL,
+  `text` longtext,
+  `user` int(11) NOT NULL,
+  PRIMARY KEY (`iddocument`),
+  KEY `user_idx` (`user`),
+  CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document`
+--
+
+LOCK TABLES `document` WRITE;
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+INSERT INTO `document` VALUES (1,'primo documento di prova ',1),(2,'secondo documento inserito dall\'utente',2),(3,'terzo documento per provare l\'inserimento',1),(4,'quarto documento inserito dall\'utente',2);
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `searchconnections`
+--
+
+DROP TABLE IF EXISTS `searchconnections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `searchconnections` (
+  `iddocument` int(11) NOT NULL,
+  `idtag` int(11) NOT NULL,
+  KEY `iddocument_idx` (`iddocument`),
+  KEY `idtags_idx` (`idtag`),
+  CONSTRAINT `iddocument` FOREIGN KEY (`iddocument`) REFERENCES `document` (`iddocument`),
+  CONSTRAINT `idtags` FOREIGN KEY (`idtag`) REFERENCES `tags` (`idtags`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `searchconnections`
+--
+
+LOCK TABLES `searchconnections` WRITE;
+/*!40000 ALTER TABLE `searchconnections` DISABLE KEYS */;
+INSERT INTO `searchconnections` VALUES (1,1),(1,3),(2,1),(2,2),(3,1),(3,3),(4,1),(4,2);
+/*!40000 ALTER TABLE `searchconnections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tags` (
+  `idtags` int(11) NOT NULL,
+  `tag` varchar(45) NOT NULL,
+  PRIMARY KEY (`idtags`),
+  UNIQUE KEY `tag_UNIQUE` (`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (1,'documento'),(3,'prova'),(2,'utente');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -28,9 +105,9 @@ CREATE TABLE `user` (
   `username` varchar(16) NOT NULL,
   `usertype` varchar(255) DEFAULT NULL,
   `password` varchar(32) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,4 +129,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-12 13:10:18
+-- Dump completed on 2020-05-22  9:29:43
