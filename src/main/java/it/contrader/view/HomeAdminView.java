65ab130cf06cd13c95d814cf740d41732a0573b7
fellonemarco <65163898+fellonemarco@users.noreply.github.com>
2020-05-22 13:05:35 +0200

    grammar correction

--- src/main/java/it/contrader/view/HomeAdminView.java
+++ src/main/java/it/contrader/view/HomeAdminView.java
@@ -34,7 +34,7 @@ public class HomeAdminView extends AbstractView {
     public void showOptions() {
         System.out.println("-------------MENU------------\n");
         System.out.println(" Seleziona cosa vuoi gestire:");
-        System.out.println("[U]tenti [T]ags [D]ocuments [S]earhconnections [E]sci");
+        System.out.println("[U]tenti [T]ags [D]ocuments [S]earchconnections [E]sci");
         //Il metodo che salva l'input nella stringa choice.
         //getInput() è definito in AbstractView.
         choice = this.getInput();
