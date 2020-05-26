a44725ef40f025952ac14413728d566f95d4cec6
Marcello <Marcello@DESKTOP-3GJQ87B>
2020-05-26 10:02:26 +0200

    Created Document and DocumentDAO

---
+++ src/main/java/it/contrader/dao/DocumentDAO.java
@@ -0,0 +1,124 @@
+package it.contrader.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.model.Document;
+import it.contrader.model.User;
+import it.contrader.utils.ConnectionSingleton;
+
+public class DocumentDAO implements DAO<Document> {
+	
+	private final String QUERY_ALL = "SELECT * FROM document";
+	private final String QUERY_CREATE = "INSERT INTO document (text) VALUES (?)";
+	private final String QUERY_READ = "SELECT * FROM document WHERE id=?";
+	private final String QUERY_UPDATE = "UPDATE document SET text=? WHERE id=?";
+	private final String QUERY_DELETE = "DELETE FROM document WHERE id=?";
+
+	@Override
+	public List<Document> getAll() {
+		List<Document> documentList = new ArrayList<>();
+		Connection connection = ConnectionSingleton.getInstance();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(QUERY_ALL);
+			Document document;
+			while (resultSet.next()) {
+				int userId = resultSet.getInt("id");
+				User userDocument = new User(null, null, null);
+				userDocument.setId(userId);
+				String text = resultSet.getString("text");
+				
+				document = new Document(userDocument, text);
+				document.setIdDocument(resultSet.getInt("id"));
+				documentList.add(document);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return documentList;
+	}
+
+	@Override
+	public Document read(int id) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_READ);
+			preparedStatement.setInt(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet.next();
+			
+			String user, text;
+			
+			text = resultSet.getString("text");
+			int userId = resultSet.getInt("id");
+			User userDocument = new User(null, null, null);
+			userDocument.setId(userId);
+			
+			Document document = new Document(userDocument, text);
+			
+			document.setIdDocument(resultSet.getInt("id"));
+
+
+			return document;
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean insert(Document documentToInsert) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {	
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_CREATE);
+			preparedStatement.setString(1, documentToInsert.getText());
+			preparedStatement.execute();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(Document documentToUpdate) {
+		Connection connection = ConnectionSingleton.getInstance();
+
+		// Check if id is present
+		if (documentToUpdate.getIdDocument() == 0)
+			return false;
+
+		Document documentRead = read(documentToUpdate.getIdDocument());
+		if (!documentRead.equals(documentToUpdate)) {
+			try {
+				// Fill the userToUpdate object
+				if (documentToUpdate.getText() == null || documentToUpdate.getText().equals("")) {
+					documentToUpdate.setText(documentRead.getText());
+				}
+
+				// Update the user
+				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(QUERY_UPDATE);
+				int a = preparedStatement.executeUpdate();
+				if (a > 0)
+					return true;
+				else
+					return false;
+
+			} catch (SQLException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/it/contrader/model/Document.java
@@ -0,0 +1,80 @@
+package it.contrader.model;
+
+public class Document {
+	
+	private int idDocument;
+	
+	private User user;
+	
+	private String text;
+
+	public Document(int idDocument, User user, String text) {
+		super();
+		this.idDocument = idDocument;
+		this.user = user;
+		this.text = text;
+	}
+
+	public Document(User user, String text) {
+		super();
+		this.user = user;
+		this.text = text;
+	}
+
+	public int getIdDocument() {
+		return idDocument;
+	}
+
+	public void setIdDocument(int idDocument) {
+		this.idDocument = idDocument;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (idDocument != other.idDocument)
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Document [idDocument=" + idDocument + ", user=" + user + ", text=" + text + "]";
+	}
+	
+	
+
+}
