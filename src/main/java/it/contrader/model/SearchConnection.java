250b42b4797eacbb3553be2420644a64d2d60948
fellonemarco <65163898+fellonemarco@users.noreply.github.com>
2020-06-04 10:00:12 +0200

    Debugging for running

--- src/main/java/it/contrader/model/SearchConnection.java
+++ src/main/java/it/contrader/model/SearchConnection.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class SearchConnection {
 
+	@Id
 	@Column (name = "idTag")
 	private int idDocument;
 	private int idTag;
--- src/main/java/it/contrader/model/User.java
+++ src/main/java/it/contrader/model/User.java
@@ -39,7 +39,7 @@ public class User {
 	
 	
 	@OneToMany(mappedBy = "user")
-	@JoinColumn(name = "user_id")
+
 	private List<Document> documentList;
 	
 
