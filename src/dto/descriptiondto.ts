2b9577e6acdd6eb3605d1b17e5c12c0f0ac28225
Marknik94 <marcello.nichele@gmail.com>
2020-07-15 16:39:03 +0200

    modified descriptions components and dto classes

--- src/dto/descriptiondto.ts
+++ src/dto/descriptiondto.ts
@@ -1,19 +1,8 @@
-import { FolderDTO } from './folderdto';
-import { LibraryDTO } from './librarydto';
-import { DocumentDTO } from './documentdto';
-import { InterestDTO } from './interestdto';
+
 
 export class DescriptionDTO {
 
     id : number;
 
     description : string;
-
-    folder : FolderDTO;
-
-    library : LibraryDTO;
-
-    document : DocumentDTO;
-
-    interest : InterestDTO;
 }
