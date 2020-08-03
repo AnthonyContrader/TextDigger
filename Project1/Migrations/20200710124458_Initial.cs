c2a9a75f112d5bda6a75f757838353e763e0ea50
fellonemarco <65163898+fellonemarco@users.noreply.github.com>
2020-08-03 17:18:40 +0200

    Stripped old files

--- Project1/Migrations/20200710124458_Initial.cs
+++ Project1/Migrations/20200710124458_Initial.cs
@@ -91,9 +91,102 @@ namespace Project1.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_Review_User_UserId",
+                        name: "FK_Subscribe_User_UserId",
                         column: x => x.UserId,
                         principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+				
+				migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(maxLength: 50, nullable: false),
+					Text = table.Column<string>(maxLength: 50, nullable: false),
+                    FolderId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Document_Folder_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "Folder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Document_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+				
+				migrationBuilder.CreateTable(
+                name: "Folder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    LibraryId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Folder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Folder_Library_LibraryId",
+                        column: x => x.LibraryId,
+                        principalTable: "Library",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+				
+				migrationBuilder.CreateTable(
+                name: "Library",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    InterestId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Library", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Library_Interest_InterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interest",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+				
+				migrationBuilder.CreateTable(
+                name: "SearchConnection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TagId = table.Column<int>(nullable: false),
+					DocumentId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SearchConnection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SearchConnection_Tag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SearchConnection_Document_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "Document",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
@@ -112,6 +205,36 @@ namespace Project1.Migrations
                 name: "IX_Subscribe_InterestGroupId",
                 table: "Subscribe",
                 column: "InterestGroupId");
+			
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchConnection_TagId",
+                table: "SearchConnection",
+                column: "SearchConnectionId");
+				
+            migrationBuilder.CreateIndex(
+                name: "IX_SearchConnection_DocumentId",
+                table: "SearchConnection",
+                column: "DocumentId");
+				
+            migrationBuilder.CreateIndex(
+                name: "IX_Library_InterestId",
+                table: "Library",
+                column: "InterestId");
+				
+            migrationBuilder.CreateIndex(
+                name: "IX_Folder_LibraryId",
+                table: "Folder",
+                column: "LibraryId");
+				
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_UserId",
+                table: "Document",
+                column: "UserId");
+				
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_FolderId",
+                table: "Document",
+                column: "FolderId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -130,6 +253,18 @@ namespace Project1.Migrations
 
             migrationBuilder.DropTable(
                 name: "Tag");
+				
+            migrationBuilder.DropTable(
+                name: "Document");
+				
+            migrationBuilder.DropTable(
+                name: "Folder");
+				
+            migrationBuilder.DropTable(
+                name: "SearchConnection");
+				
+            migrationBuilder.DropTable(
+                name: "Library");
         }
     }
 }
