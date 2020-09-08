45373b274e9cee96fa526fa985a91aada9f7db0f
Marknik94 <marcello.nichele@gmail.com>
2020-09-08 17:24:52 +0200

    Revert "First Commit"
    This reverts commit 2879a3f11e1e45d7924e59f2dc9f00a84631f45b.

---
+++ src/main/java/com/contrader/textdigger/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.contrader.textdigger.security;
+
+import com.contrader.textdigger.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
