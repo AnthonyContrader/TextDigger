2879a3f11e1e45d7924e59f2dc9f00a84631f45b
Marknik94 <marcello.nichele@gmail.com>
2020-09-08 17:18:37 +0200

    First Commit
    Creation of textdigger microservice and gateway

--- src/main/java/com/contrader/textdigger/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.contrader.textdigger.web.rest;
--- src/main/java/com/contrader/textdigger/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.contrader.textdigger.web.rest.vm;
