e59f8bf41b025d4a1a2b5e4d2e892732fa06eaf3
Marknik94 <marcello.nichele@gmail.com>
2020-09-09 15:20:25 +0200

    modified textdigger

---
+++ gateway/src/main/java/com/contrader/textdigger/config/GatewayConfiguration.java
@@ -0,0 +1,32 @@
+package com.contrader.textdigger.config;
+
+import io.github.jhipster.config.JHipsterProperties;
+
+import com.contrader.textdigger.gateway.accesscontrol.AccessControlFilter;
+import com.contrader.textdigger.gateway.responserewriting.SwaggerBasePathRewritingFilter;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Configuration
+    public static class SwaggerBasePathRewritingConfiguration {
+
+        @Bean
+        public SwaggerBasePathRewritingFilter swaggerBasePathRewritingFilter() {
+            return new SwaggerBasePathRewritingFilter();
+        }
+    }
+
+    @Configuration
+    public static class AccessControlFilterConfiguration {
+
+        @Bean
+        public AccessControlFilter accessControlFilter(RouteLocator routeLocator, JHipsterProperties jHipsterProperties) {
+            return new AccessControlFilter(routeLocator, jHipsterProperties);
+        }
+    }
+
+}
