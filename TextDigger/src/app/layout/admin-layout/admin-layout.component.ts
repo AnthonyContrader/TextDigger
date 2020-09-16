73acdb294e618115ccb2565753d2b6d71e6b8e45
Marknik94 <marcello.nichele@gmail.com>
2020-09-16 09:08:06 +0200

    modified app-routing module.ts

--- TextDigger/src/app/layout/admin-layout/admin-layout.component.ts
+++ TextDigger/src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/dto/userdto';
 
 @Component({
   selector: 'app-admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.css']
+  templateUrl: './admin-layout.component.html'
 })
 export class AdminLayoutComponent implements OnInit {
-
+  user: UserDTO = new UserDTO;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('currentUserData'));
+    let auth = JSON.parse(localStorage.getItem('currentUser'));
+    if (auth == null){
+      window.location.href = '/login';
+    }
+  }
+
+  logout(){
+    localStorage.removeItem('currentUser');
+    window.location.href = '/login';
   }
 
 }
